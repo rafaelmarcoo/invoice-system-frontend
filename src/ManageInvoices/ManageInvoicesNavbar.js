b1f84dbf3af48f9f59937d1bf6d94ffb5d666299
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-11-28 11:55:36 +1300

    Update ManageInvoicesNavbar.js

--- src/ManageInvoices/ManageInvoicesNavbar.js
+++ src/ManageInvoices/ManageInvoicesNavbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const ManageInvoicesNavbar = () => {
     return (
         <div className="manage-invoices-navbar">
-            <Link>Sent Invoices</Link>
+            <Link to="/manageinvoices/sent">Sent Invoices</Link>
             <Link>Paid Invoices</Link>
             <Link>Overdue Invoices</Link>
             {/*Either separate pages orrrr separate components using states*/}
