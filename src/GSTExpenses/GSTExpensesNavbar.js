f62234d0f38c6dfaccb4026579034cb33e8bde9c
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-11-28 12:34:09 +1300

    Create GSTExpensesNavbar.js

---
+++ src/GSTExpenses/GSTExpensesNavbar.js
@@ -0,0 +1,8 @@
+export const GSTExpensesNavbar = () => {
+    return (
+        <div className="gst-expenses-navbar">
+            <button>Add GST Expense</button>
+            <button>All GST Expenses</button>
+        </div>
+    );
+};
