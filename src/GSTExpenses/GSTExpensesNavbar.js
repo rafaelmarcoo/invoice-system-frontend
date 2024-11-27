a7613f2cea4db1dd11c80e79eee4de3975aa0d5f
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-11-28 12:54:27 +1300

    Edited className and applied styling on secondary navbars

--- src/GSTExpenses/GSTExpensesNavbar.js
+++ src/GSTExpenses/GSTExpensesNavbar.js
@@ -1,6 +1,6 @@
 export const GSTExpensesNavbar = (props) => {
     return (
-        <div className="gst-expenses-navbar">
+        <div className="secondary-navbar">
             <button onClick={() => props.toggleTable("add")}>Add GST Expense</button>
             <button onClick={() => props.toggleTable("view")}>All GST Expenses</button>
         </div>
--- src/ManageInvoices/ManageInvoicesNavbar.js
+++ src/ManageInvoices/ManageInvoicesNavbar.js
@@ -1,6 +1,6 @@
 export const ManageInvoicesNavbar = (props) => {
     return (
-        <div className="manage-invoices-navbar">
+        <div className="secondary-navbar">
             <button onClick={() => props.toggleTable("sent")}>Sent Invoices</button>
             <button onClick={() => props.toggleTable("paid")}>Paid Invoices</button>
             <button onClick={() => props.toggleTable("overdue")}>Overdue Invoices</button>
