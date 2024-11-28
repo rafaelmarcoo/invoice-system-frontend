9c747e0ba982f7728ed35e4464dd189383a8ca75
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-11-28 14:24:14 +1300

    Updated CompanyForm.js with states etc

--- src/EditCompany/CompanyForm.js
+++ src/EditCompany/CompanyForm.js
@@ -1,27 +1,103 @@
+import { useState } from "react";
+import { Axios } from "axios";
+
 export const CompanyForm = () => {
+    const [formData, setFormData] = useState({
+        companyName: "",
+        gstNumber: "",
+        companyAddress: "",
+        companyCity: "",
+        companyZip: "",
+        companyPhone: "",
+        companyEmail: ""
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        console.log(formData);
+    }
+
     return (
         <div className="form-container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Company Name</label>
-                <input type="text" placeholder="Company Name..." required/>
+                <input 
+                    type="text"
+                    name="companyName"
+                    value={formData.companyName}
+                    onChange={handleChange} 
+                    placeholder="Company Name..." 
+                    required
+                />
 
                 <label>GST Number</label>
-                <input type="text" placeholder="Email..." required/>
+                <input 
+                    type="text" 
+                    name="gstNumber"
+                    value={formData.gstNumber}
+                    onChange={handleChange}
+                    placeholder="Email..." 
+                    required
+                />
 
                 <label>Company Address</label>
-                <input type="text" placeholder="Company Address..." required/>
+                <input 
+                    type="text" 
+                    name="companyAddress"
+                    value={formData.companyAddress}
+                    onChange={handleChange}
+                    placeholder="Company Address..." 
+                    required
+                />
 
                 <label>Company City</label>
-                <input type="text" placeholder="Company City" required/>
+                <input 
+                    type="text" 
+                    name="companyCity"
+                    value={formData.companyCity}
+                    onChange={handleChange}
+                    placeholder="Company City" 
+                    required
+                />
 
                 <label>Company ZIP</label>
-                <input type="text" id="company-zip" name="companyZip" placeholder="Company Zip" required/>
+                <input 
+                    type="text" 
+                    name="companyZip" 
+                    value={formData.companyZip}
+                    onChange={handleChange}
+                    placeholder="Company Zip" 
+                    required
+                />
 
                 <label>Company Phone</label>
-                <input type="tel" placeholder="Phone..." required/>
+                <input 
+                    type="tel" 
+                    name="companyPhone"
+                    value={formData.companyPhone}
+                    onChange={handleChange}
+                    placeholder="Phone..." 
+                    required
+                />
 
                 <label>Company Email</label>
-                <input type="email" placeholder="Email..." required/>
+                <input 
+                    type="email" 
+                    name="companyEmail"
+                    value={formData.companyEmail}
+                    onChange={handleChange}
+                    placeholder="Email..." 
+                    required
+                />
 
                 <br/><br/>
                 <button>Save Changes</button>
