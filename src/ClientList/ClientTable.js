0005753d47a1db450b240b4f1ae3cc9f98f89661
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-11-29 17:33:04 +1300

    Update ClientTable.js

--- src/ClientList/ClientTable.js
+++ src/ClientList/ClientTable.js
@@ -1,19 +1,52 @@
+import Axios from "axios";
+import { useEffect, useState } from "react";
+
 export const ClientTable = () => {
+    const [clients, setClients] = useState([]);
+
+    useEffect(() => {
+        const retrieveClients = async () => {
+            try {
+                const response = await Axios.get("http://localhost:5041/api/client");
+                setClients(response.data);
+            } catch(error) {
+                alert("Error: " + error.message);
+            }
+        };
+
+        retrieveClients();
+    }, []);
+
     return (
         <div className="client-list-table">
             <h3>Client List</h3>
             <table>
                 <thead>
                     <tr>
-                        <th>Client Name</th>
+                        <th>Name</th>
+                        <th>Code</th>
+                        <th>GST Number</th>
                         <th>Email</th>
-                        <th>Recurring Invoices</th>
-                        <th>Total Items</th>
+                        <th>Phone</th>
+                        <th>Address</th>
                         <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {/* Will add data from database */}
+                    {clients.length > 0 ? (
+                        clients.map((client, index) => (
+                            <tr key={index}>
+                                <td>{client.name}</td>
+                                <td>{client.company_code}</td>
+                                <td>{client.gst_number}</td>
+                                <td>{client.email}</td>
+                                <td>{client.phone}</td>
+                                <td>{client.address}</td>
+                                <td><button key={index}>Action</button></td>
+                            </tr>
+                        )
+
+                    )) : (<tr><td colSpan="5">NO CLIENTS</td></tr>)}
                 </tbody>
             </table>    
         </div>
