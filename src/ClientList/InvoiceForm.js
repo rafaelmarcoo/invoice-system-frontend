336b9320a3941e9186e9a4f75c8e06a33ed3c727
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-12-04 12:21:03 +1300

    Update InvoiceForm.js

--- src/ClientList/InvoiceForm.js
+++ src/ClientList/InvoiceForm.js
@@ -1,26 +1,49 @@
 import { useState } from "react";
 
 export const InvoiceForm = ({ clients }) => {
-    const [items, setItems] = useState([]);
-    const [isRecurring, setIsRecurring] = useState("no");
+    const [formData, setFormData] = useState({
+        name: "",
+        frequency: "",
+        dateDue: "",
+        items: [],
+    });
 
-    const handleRecurringChange = (event) => {
-        setIsRecurring(event.target.value);
-    }
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
 
     const addItem = () => {
-        const item = {
-            id: items.length === 0 ? 1 : items[items.length - 1].id + 1,
+        const newItem = {
+            id: formData.items.length === 0 ? 1 : formData.items[formData.items.length - 1].id + 1,
             description: "",
             quantity: "",
             price: "",
-        }
-        setItems([...items, item]);;
+        };
+
+        setFormData({
+            ...formData,
+            items: [...formData.items, newItem],
+        });
     };
 
     const removeItem = (removeId) => {
-        const newItemsArr = items.filter((item) => item.id !== removeId);
-        setItems(newItemsArr);
+        setFormData({
+            ...formData,
+            items: formData.items.filter((item) => item.id !== removeId)
+        });
+    };
+
+    const handleItemChange = (id, field, value) => {
+        setFormData({
+            ...formData,
+            items: formData.items.map((item) => 
+                item.id === id ? {...item, [field]: value } : item
+            ),
+        });
     };
 
     return (
@@ -28,7 +51,12 @@ export const InvoiceForm = ({ clients }) => {
             <h3>New Invoice Form</h3>
 
             <label>Client</label>
-            <select className="drop-down" name="clients" id="clients">
+            <select 
+                className="drop-down" 
+                name="name" 
+                onChange={handleChange}
+                value={formData.name}
+            >
                 {clients.length > 0 ? (
                     clients.map((client, index) => (
                         <option key={index} value={client.companyCode}>
@@ -40,34 +68,59 @@ export const InvoiceForm = ({ clients }) => {
 
             <br/><br/>
             
-            <label>Date</label>
-            <input type="date"></input>
-
-            <label>Is recurring?</label>
-            <select value={isRecurring} onChange={handleRecurringChange}>
-                <option value="no">No</option>
-                <option value="yes">Yes</option>
+            <label>Frequency</label>
+            <select
+                className="drop-down"
+                name="frequencies"
+                onChange={handleChange}
+                value={formData.frequency}
+            >    
+                <option value="one-time">One-Time</option>
+                <option value="weekly">Weekly</option>
+                <option value="fortnightly">Fortnightly</option>
+                <option value="monthly">Monthly</option>
             </select>
+
+            <br/><br/>
+
+            <label>Due Date</label>
+            <input 
+                type="date"
+                name="dateDue"
+                onChange={handleChange}
+                value={formData.dateDue}
+            >
+
+            </input>
 
             <br/><br/>
 
             <label>Items</label>
             <br/>
-            {items.map((item) => (
+            {formData.items.map((item) => (
                 <div key={item.id} className="item">
                     <label>Description</label>
-                    <input 
+                    <input
+                        name="Description" 
                         type="text"
+                        value={item.description}
+                        onChange={(e) => handleItemChange(item.id, "description", e.target.value)}
                     />
 
-                    <label>quantity</label>
-                    <input 
+                    <label>Quantity</label>
+                    <input
+                        name="Quantity" 
                         type="text"
+                        value={item.quantity}
+                        onChange={(e) => handleItemChange(item.id, "quantity", e.target.value)}
                     />
 
                     <label>Price</label>
                     <input 
+                        name="Price"
                         type="text"
+                        value={item.price}
+                        onChange={(e) => handleItemChange(item.id, "price", e.target.value)}
                     />
 
                     <button onClick={() => removeItem(item.id)}>X</button>
