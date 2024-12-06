279cc7da0af80653014286a91bdf56224b834944
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-12-06 15:14:27 +1300

    Grouped Paid Invoices

--- src/ManageInvoices/PaidInvoices.js
+++ src/ManageInvoices/PaidInvoices.js
@@ -1,11 +1,52 @@
 export const PaidInvoices = (props) => {
     const paidInv = props.invoices.filter(invoice => invoice.status === "Paid");
 
+    const groupedInv = props.invoices
+        .filter(invoice => invoice.status === "Paid")
+        .reduce((acc, invoice) => {
+            if(!acc[invoice.name]) {
+                acc[invoice.name] = [];
+            }
+            acc[invoice.name].push(invoice);
+            return acc;
+        }, {});
+
     return (
         <div className="manage-invoices-paid-invoices">
             <h3>Paid Invoices</h3>
 
-            <table>
+
+            {Object.keys(groupedInv).length > 0 ? (
+                Object.keys(groupedInv).map((companyCode) => (
+                    <div key={companyCode}>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Client Code</th>
+                                    <th>Invoice Number</th>
+                                    <th>Date Sent</th>
+                                    <th>Date Due</th>
+                                    <th>Total Amount</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {groupedInv[companyCode].map((invoice) => (
+                                    <tr key={invoice.id}>
+                                        <td>{invoice.name}</td>
+                                        <td>{invoice.id}</td>
+                                        <td>{invoice.dateSent}</td>
+                                        <td>{invoice.dateDue}</td>
+                                        <td>${invoice.amount}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                ))
+            ) : (<tr><td colSpan="6">NO PAID INVOICES</td></tr>)}
+
+
+            {/* <table>
                 <thead>
                     <tr>
                         <th>Client Code</th>
@@ -28,7 +69,7 @@ export const PaidInvoices = (props) => {
                         )
                     )) : (<tr><td colSpan="6">NO PAID INVOICES</td></tr>)}
                 </tbody>
-            </table>
+            </table> */}
         </div>
     );
 }
