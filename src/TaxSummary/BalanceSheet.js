196516ed41d35c25cb475eaa852d4f7b792ffdf6
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-12-23 18:24:25 +1300

    Balance Sheet and Income Statement

--- src/TaxSummary/BalanceSheet.js
+++ src/TaxSummary/BalanceSheet.js
@@ -1,7 +1,16 @@
-export const BalanceSheet = () => {
+export const BalanceSheet = (props) => {
     return (
         <div>
-            
+            <table>
+                <thead>
+                    <tr>
+                        <th>Balance Sheet</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
         </div>
     );
 }
--- src/TaxSummary/IncomeStatement.js
+++ src/TaxSummary/IncomeStatement.js
@@ -1,7 +1,23 @@
 export const IncomeStatement = () => {
     return (
         <div>
-            
+            <table>
+                <thead>
+                    <tr>
+                        <th>Income Statement</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                    <tr>
+                        <td>Revenue:</td>
+                    </tr>
+                    <tr>
+                        <td>Services</td>
+                    </tr>
+                <tbody>
+
+                </tbody>
+            </table>
         </div>
     );
 }
